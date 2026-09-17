0757d63ba5a6274d3b6c254a4f0fc769312239e8
edulimac <h2n46jdsch@privaterelay.appleid.com>
2026-09-16 22:46:34 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: "COLE_AQUI_A_URL_DO_SUPABASE",
-  SUPABASE_ANON_KEY: "COLE_AQUI_A_CHAVE_PUBLICA_DO_SUPABASE"
-};
-
-
-window.APP_CONFIG = {
   SUPABASE_URL: "https://drtjuvxawfbjeakwlqyp.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_iL3ROG6M9kBtTN-WhbF_hw_x-0Q1CJN"
 };
